583ce6c697472f70a5d8413cee8e60b200305ce5
Tobin Yehle <tobinyehle@gmail.com>
2015-11-12 11:35:22 -0700

    add illinois libs

---
+++ src/cs/utah/sherlock/demos/IllinoisChunker.java
@@ -0,0 +1,15 @@
+package cs.utah.sherlock.demos;
+
+import edu.illinois.cs.cogcomp.lbj.chunk.Chunker;
+import edu.illinois.cs.cogcomp.lbjava.nlp.seg.Token;
+
+/**
+ * @author Tobin Yehle
+ */
+public class IllinoisChunker {
+    public static void main(String[] args) {
+        Chunker chunker = new Chunker();
+        Token word;
+        System.out.println("Done");
+    }
+}
